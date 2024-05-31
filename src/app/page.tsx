7d096dcb302103e3dfb32b233232aa507652a831
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-05-31 17:35:08 +0700

    chore: add axios & react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import styles from './page.module.css';
 import LoginPage from './components/loginPage/loginPage';
+import TodoPage from './components/todoPage/todoPage';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Haloo</h1>
-      <LoginPage />
+      <TodoPage />
     </main>
   );
 }
